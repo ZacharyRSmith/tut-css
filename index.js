ef2a67f393e165d4d3e3cb6aecfaf6fbb26acba1
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-07-03 09:09:05 -0400

    awesome: Make it so!

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var server = express();
+var PORT = process.env.PORT || '3000';
+
+server.use(express.static('public'));
+server.get('/', function (req, res) {
+  res.sendFile('./index.html', { root: __dirname });
+});
+
+server.listen(PORT, function () {
+  console.log('Server listening on port', PORT);
+});
