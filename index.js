73960e5e1f5df159d77fc316efef85681ffebeb8
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-07-03 09:24:07 -0400

    enhance: Make express look up dir paths dynamically

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var server = express();
 var PORT = process.env.PORT || '3000';
 
-server.use(express.static('public'));
+server.use(express.static('./public', { root: __dirname }));
+
 server.get('/', function (req, res) {
   res.sendFile('./index.html', { root: __dirname });
 });
